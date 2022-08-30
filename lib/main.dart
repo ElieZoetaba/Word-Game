65ecc7184677674afd7fe59f79985b017e8a069d
[ElieZoetaba] <[zoetabaelie@gmail.com]>
2022-08-30 01:49:15 +0000

    End
    Signed-off-by: [ElieZoetaba] <[zoetabaelie@gmail.com]>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/game_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,13 +12,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(home: GameScreen(),);
   }
 }
-
--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,1 +1,46 @@
-imp
+import 'package:flutter/material.dart';
+import 'package:game/utilis/game_provider.dart';
+import 'package:game/widgets/game_keyboard.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  WorldeGame _game = WorldeGame();
+  late String word;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WorldeGame.initGame();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF212121),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            "Wordle",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          GameKeyboard(_game),
+        ],
+      ),
+    );
+  }
+}
